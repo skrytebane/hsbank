f680c03fc9f650ea02b3497a1015db5d9e3864d0
Johannes Groedem <johs@grdm.no>
2018-02-02 23:09:22 +0100

    Show balances in a semi-nice way.
    Also swap Yaml for JSON secrets file.

--- Main.hs
+++ Main.hs
@@ -2,33 +2,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Lens
+import           Data.Aeson
 import           Data.Aeson.Lens      (key, _String)
+import           Data.Maybe           (maybe)
 import           Data.String          (fromString)
+import           Data.Text            (Text)
 import qualified Data.Text            as T
-import           Data.Yaml
 import           GHC.Generics
 import           Network.Wreq
 import qualified Network.Wreq.Session as S
 import           System.Environment   (getEnv)
 import           System.FilePath      ((</>))
-
-tokenUrl :: String
-tokenUrl = "https://api.sbanken.no/identityserver/connect/token"
+import           System.IO
 
 data Config = Config
-  {
-    customerId :: String
-  , apiKey     :: String
-  , secret     :: String
-  } deriving (Show, Generic)
+  { customer :: Text
+  , apiKey   :: Text
+  , secret   :: Text
+  }
+  deriving (Show, Generic)
 
 instance FromJSON Config
 
 requestBearerToken :: S.Session -> Config -> IO String
 requestBearerToken sess (Config _ u p) = do
-  let opts = defaults & auth ?~ basicAuth (fromString u) (fromString p)
-                      & header "User-Agent" .~ ["hsbank"]
-  res <- S.postWith opts sess tokenUrl
+  let opts = defaults & auth ?~ basicAuth (fromString $ T.unpack u)
+                                          (fromString $ T.unpack p)
+  res <- S.postWith opts sess "https://api.sbanken.no/identityserver/connect/token"
     [ "grant_type" := ("client_credentials"::String) ]
   let type' = res ^? responseBody . key "token_type" . _String
   case type' of
@@ -38,20 +38,63 @@ requestBearerToken sess (Config _ u p) = do
         Nothing -> fail "No bearer token!"
     _ -> fail $ "Expecting bearer token, got " ++ show type'
 
-getAccounts :: S.Session -> String -> Config -> IO (Maybe Value)
+data AccountResult = AccountResult
+  {
+    availableItems :: Int
+  , items          :: [Account]
+  , errorType      :: Maybe Text
+  , isError        :: Bool
+  , errorMessage   :: Maybe Text
+  , traceId        :: Maybe Text
+  } deriving (Show, Generic)
+
+instance FromJSON AccountResult
+
+data Account = Account
+  {
+    ownerCustomerId :: Text
+  , balance         :: Double
+  , customerId      :: Text
+  , name            :: Text
+  , defaultAccount  :: Bool
+  , accountNumber   :: Text
+  , creditLimit     :: Double
+  , accountType     :: Text
+  , available       :: Double
+  } deriving (Show, Generic)
+
+instance FromJSON Account
+
+getAccounts :: S.Session -> String -> Config -> IO (Maybe AccountResult)
 getAccounts sess token (Config uid _ _) = do
   let opts = defaults & auth ?~ oauth2Bearer (fromString token)
   res <- S.getWith opts sess $
-    "https://api.sbanken.no/bank/api/v1/Accounts/" ++ uid
-  return $ res ^? responseBody . key "items"
+    "https://api.sbanken.no/bank/api/v1/Accounts/" ++ T.unpack uid
+  case eitherDecode <$> res ^? responseBody of
+    Just (Right parsed) ->
+      return parsed
+    Just (Left e) ->
+      fail e
+    Nothing ->
+      fail "No response body!"
 
 readConfig :: IO Config
 readConfig = do
   home <- getEnv "HOME"
-  res <- decodeFile $ home </> "secrets" </> "sbanken.yaml"
-  case res of
-    Nothing  -> fail "Invalid or non-existent secrets file."
-    Just cfg -> return cfg
+  let path = home </> "secrets" </> "sbanken.json"
+  withFile path ReadMode $
+    \handle ->
+      do s <- hGetContents handle
+         case eitherDecode $ fromString s of
+           Left e    -> fail e
+           Right cfg -> return cfg
+
+printBalances :: [Account] -> IO ()
+printBalances =
+  mapM_ printBalance
+  where printBalance (Account _ bal _ name' _ num _ _ avail) =
+          putStrLn $ T.unpack num ++ ": " ++ T.unpack name' ++
+          ": " ++ show bal ++ " (saldo: " ++ show avail ++ ")"
 
 main :: IO ()
 main = do
@@ -59,4 +102,4 @@ main = do
   sess <- S.newSession
   token <- requestBearerToken sess cfg
   accs <- getAccounts sess token cfg
-  print accs
+  printBalances $ maybe [] items accs
